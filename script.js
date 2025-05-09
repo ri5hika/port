444b2ee5a89823d5988e1aa74fbff11f2c3a0b2a
Rishika Singh <144714429+ri5hika@users.noreply.github.com>
2025-05-10 01:23:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const imageMap = {
+    1: ['myop1.jpg', 'myop3.jpg','myop3.jpg','myop4.jpg'],
+    2: ['z.png', 'z1.png', 'z2.png'],
+    3: ['tar1.jpg', 'tar2.jpg', 'tar3.jpg', 'tar4.jpg'],
+    4: ['yuri1.jpg', 'yuri2.jpg', 'yuri3.jpg']
+};
+
+const currentIndices = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0
+};
+
+function showImage(carouselId) {
+    const imgElement = document.getElementById(`carousel${carouselId}-img`);
+    imgElement.src = imageMap[carouselId][currentIndices[carouselId]];
+}
+
+function nextImage(carouselId) {
+    currentIndices[carouselId] = (currentIndices[carouselId] + 1) % imageMap[carouselId].length;
+    showImage(carouselId);
+}
+
+function prevImage(carouselId) {
+    currentIndices[carouselId] = (currentIndices[carouselId] - 1 + imageMap[carouselId].length) % imageMap[carouselId].length;
+    showImage(carouselId);
+}
