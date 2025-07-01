d83730346c21a03b6435342f9b9f5c5ca82e892f
Rishika Singh <144714429+ri5hika@users.noreply.github.com>
2025-07-02 01:18:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ const imageMap = {
     1: ['myop1.jpg', 'myop3.jpg','myop3.jpg','myop4.jpg'],
     2: ['z.png', 'z1.png', 'z2.png'],
     3: ['tar1.jpg', 'tar2.jpg', 'tar3.jpg', 'tar4.jpg'],
-    4: ['yuri1.jpg', 'yuri2.jpg', 'yuri3.jpg']
+    4: ['yuri1.jpg', 'yuri2.jpg', 'yuri3.jpg'],
+    5: ['voi1.jpg', 'voi2.png', 'voi3.png', 'voi4.png']
 };
 
 const currentIndices = {
     1: 0,
     2: 0,
     3: 0,
-    4: 0
+    4: 0,
+    5: 0
 };
 
 function showImage(carouselId) {
